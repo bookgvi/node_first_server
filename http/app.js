c02347877878a0f20b0efc78496a953a8fc8d151
book <bookgvi@gmail.com>
2020-01-06 01:16:47 +0300

    Render страници index.html и about.html

--- http/app.js
+++ http/app.js
@@ -1,12 +1,26 @@
 const http = require('http');
 const fs = require('fs');
 
-const readStream = fs.createReadStream('http/front/index.html', 'utf-8')
+const baseURL = 'http/front/';
+
+const readStream = fs.createReadStream(`${baseURL}index.html`, 'utf-8')
 const server = http.createServer((req, res) => {
-  console.log(req.url)
   if (req.url === '/') {
-    console.log('We\'re on root...')
     readStream.pipe(res)
+  } else if (req.url === '/about') {
+    fs.readFile(`${baseURL}about.html`, 'utf-8',(err, data) => {
+      if (err) {
+        res.statusCode = 404
+        res.statusMessage = 'Resource not found'
+        res.setHeader("Content-type", "text/html; charset=utf-8;")
+        res.write("<h2 style='margin-top: 5rem; text-align: center'>404 - Not found</h2>")
+        res.end()
+      } else {
+        const message = 'This is about page'
+        data = data.replace('{{ message }}', message)
+        res.end(data)
+      }
+    })
   }
 })
 server.listen('8080', () => {
